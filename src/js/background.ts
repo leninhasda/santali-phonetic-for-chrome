83458a175c91863ce4001a7349ce9640e3ea1b60
Lenin Hasda <leninhasda@gmail.com>
2025-03-25 10:36:27 +0100

    Updated manifest version and icons

--- src/js/background.ts
+++ src/js/background.ts
@@ -1,10 +1,13 @@
+let iconPath = 'images/icon-128.png';
+let iconDimPath = 'images/icon-128-dim.png';
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // console.log(request)
     if (request.isOn) {
         console.log('on')
-        chrome.pageAction.setIcon({ tabId: sender.tab.id, path: 'images/icon-48.png' });
+        chrome.pageAction.setIcon({ tabId: sender.tab.id, path: iconPath });
     } else {
-        chrome.pageAction.setIcon({ tabId: sender.tab.id, path: 'images/icon-48-dim.png' });
+        chrome.pageAction.setIcon({ tabId: sender.tab.id, path: iconDimPath });
     }
     chrome.pageAction.show(sender.tab.id);
     // sendResponse({})
